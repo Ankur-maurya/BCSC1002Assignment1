895d1c5e595067a7f8eaf4ce889cf96912359978
Ankur Mauray <Ankur.maurya_cs19@gla.ac.in>
2020-08-30 16:18:01 +0530

    now return the name of the students

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -69,4 +69,15 @@ public class Student {
         public void setStudentFirstName(String studentFirstName) {
                 this.studentFirstName = studentFirstName;
         }
+
+        /**
+         * This method returns the middle name of the student.
+         *
+         * @return The middle name of the student.
+         */
+
+        public String getStudentMiddleName() {
+                return this.studentMiddleName;
+        }
+
 }
