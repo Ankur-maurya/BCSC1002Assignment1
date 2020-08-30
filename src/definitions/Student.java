8f7e330c0ab3081ad966a43a1950370d7593fc79
Ankur Mauray <Ankur.maurya_cs19@gla.ac.in>
2020-08-30 16:10:18 +0530

    create a student method

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,10 +1,17 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  Author: Ankur Maurya (Ankur-maurya)
+ *   Date: 28/08/2020
+ *   Time: 9:18 PM
  *  File Name : Student.java
  * */
 package definitions;
 
 public class Student {
+        Student studentObject = new Student();
+        private String studentFirstName, studentMiddleName, studentLastName;
+        private long universityRollNumber;
+        private int numberOfBooksIssued;
+        private Book[] name;
+
+
 }
